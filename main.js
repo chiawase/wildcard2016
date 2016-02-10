6bf572ce5327b151007a6178c734824f14ccaa22
Chi Señires <chi.senires@gmail.com>
2016-02-10 08:52:01 +0800

    Fixed responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(window).on('scroll', function() {
-    if ($(this).scrollTop() > 75) {
+    if ($(this).scrollTop() > 75 && !(window.matchMedia("(max-width: 760px)").matches)) {
       $('.header').addClass('mini');
     } else {
       $('.header').removeClass('mini');
